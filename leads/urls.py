bd44495c666a5a9d24fa7cee1e9c530c10ec21f5
DataBasePy <ismayl.sadillaev@mail.ru>
2021-11-24 23:06:05 +0500

    fist commit

---
+++ leads/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', leads_lists),
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create)
+
+]
---
+++ leads/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .import models
+from .forms import*
+
+def leads_lists(request):
+    leads = models.Lead.objects.all()
+    context={
+        "leads":leads
+    }
+    return render(request, "leads_lists.html", context)
+
+def lead_detail(request, pk):
+    lead = get_object_or_404(models.Lead, id=pk) 
+    context={
+        "lead":lead
+    }
+    return render(request,'details.html', context)
+
+def lead_create(request):
+    forms = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            ismi = form.cleaned_data['ismi']
+            familiyasi = form.cleaned_data['familiyasi']
+            yoshi = form.cleaned_data['yoshi']
+            agent = models.Agent.objects.first()
+            models.Lead.objects.create(
+                ismi=ismi,
+                familiyasi=familiyasi,
+                yoshi=yoshi,
+                agent=agent
+            )
+    context = {
+        'forms': form
+    }
+    return render(request, 'create.html', context)
+    
+    
+
